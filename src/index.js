9cd35bd64f9e35f7989d91b146a5d4ff42f9fa8f
MarkBranton1999 <markbranton99@gmail.com>
2023-11-15 21:51:25 -0500

    got modals set up

--- src/index.js
+++ src/index.js
@@ -2,12 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import axios from "axios"
-
+import Modal from 'react-modal'
+import ArticleList from "./components/ArticleList";
+import AppContext from "./AppContext";
 import "./styles.css";
 
 const API_KEY = process.env.REACT_APP_API_KEY
 
-const App = () => (
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+const rootElement = document.getElementById("root");
+Modal.setAppElement(rootElement);
+
+function App(){
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return (
   <div>
     <ComposableMap>
       <Geographies geography="/features.json">
@@ -29,15 +59,27 @@ const App = () => (
                   headers: {
                     "X-Api-Key": API_KEY
                   }
-                }).then((res) => console.log(res.data.sources))
+                }).then((res) => {
+                  console.log(res.data.sources)
+                })
+                openModal();
               }}
             />
           ))
         }
       </Geographies>
     </ComposableMap>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        shouldCloseOnOverlayClick={true}
+      >
+        <ArticleList></ArticleList>
+      </Modal>
   </div>
-);
-
-const rootElement = document.getElementById("root");
+  );
+  }
 ReactDOM.render(<App />, rootElement);
